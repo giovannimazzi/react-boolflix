dad93424eeb14732dc5b461ed510c3c75e6f9e31
giovannimazzi <gioelor@libero.it>
2026-02-25 17:20:43 +0100

    layout refactoring

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -3,13 +3,13 @@ import SearchBar from "../components/SearchBar";
 
 export default function DefaultLayout() {
   return (
-    <>
+    <div className="d-flex flex-column vh-100">
       <SearchBar />
-      <div id="page-content">
+      <div id="page-content" className="flex-grow-1">
         <div className="container py-4">
           <Outlet />
         </div>
       </div>
-    </>
+    </div>
   );
 }
