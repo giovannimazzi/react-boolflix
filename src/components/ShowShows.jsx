80f91959ff808c1fc4e30a6725b64fc070223988
giovannimazzi <gioelor@libero.it>
2026-02-25 16:58:15 +0100

    add empty state rendering pt.2

--- src/components/ShowShows.jsx
+++ src/components/ShowShows.jsx
@@ -4,12 +4,13 @@ export default function ShowShows({ title, shows }) {
   return (
     <>
       <div className="show-title">
-        <h1 className="text-black">{title}</h1>
+        <h1 className="text-light">{title}</h1>
       </div>
       <div className="row row-cols-3 row-cols-md-4 g-3 mb-5">
         {shows?.map((show) => (
           <ShowCard key={show.id} show={show} />
         ))}
+        {shows?.length === 0 && <h5 className="text-dark">Nessun risultato</h5>}
       </div>
     </>
   );
